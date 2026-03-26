4c123c7fb70600515e54335e3e3421525c2377dd
mndiaye011 <mndiaye011@ensc.fr>
2026-03-26 11:41:43 +0100

    fix bug sur le stockage des participants

--- quiz-ui/src/services/ParticipationStorageService.js
+++ quiz-ui/src/services/ParticipationStorageService.js
@@ -1,25 +1,31 @@
 export default {
-    clear() {
-      window.localStorage.removeItem("playerName");
-      window.localStorage.removeItem("participationScore");
-    },
-    savePlayerName(playerName) {
-      window.localStorage.setItem("playerName", playerName);
-    },
-    getPlayerName() {
-      return window.localStorage.getItem("playerName");
-    },
-    saveParticipationScore(participationScore) {
-      window.localStorage.setItem("participationScore", participationScore);
-    },
-    getParticipationScore() {
-      return window.localStorage.getItem("participationScore");
-    },
-    saveTotalQuestions(total) {
-      window.localStorage.setItem("totalQuestions", total);
-    },
-    getTotalQuestions() {
-      return window.localStorage.getItem("totalQuestions");
-    }
-  };
-  
+  clear() {
+    window.localStorage.removeItem("playerName");
+    window.localStorage.removeItem("participationScore");
+  },
+  savePlayerName(playerName) {
+    window.localStorage.setItem("playerName", playerName);
+  },
+  getPlayerName() {
+    return window.localStorage.getItem("playerName");
+  },
+  saveParticipationScore(participationScore) {
+    window.localStorage.setItem("participationScore", participationScore);
+  },
+  getParticipationScore() {
+    return window.localStorage.getItem("participationScore");
+  },
+  saveTotalQuestions(total) {
+    window.localStorage.setItem("totalQuestions", total);
+  },
+  getTotalQuestions() {
+    return window.localStorage.getItem("totalQuestions");
+  },
+  saveSelectedQuiz(quiz) {
+    window.localStorage.setItem("selectedQuiz", JSON.stringify(quiz));
+  },
+  getSelectedQuiz() {
+    const q = window.localStorage.getItem("selectedQuiz");
+    return q ? JSON.parse(q) : null;
+  },
+};
